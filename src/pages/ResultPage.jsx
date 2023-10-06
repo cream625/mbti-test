1cc793ab2d4bc1a8d0cc9aa61226f2f9f879489c
cream625 <eulgeepass@gmail.com>
2023-10-06 15:17:36 +0900

    공유하기 버튼

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -3,12 +3,30 @@ import styles from "../App.module.css";
 import { resultData } from "../constants/resultData";
 import { useParams, Link } from "react-router-dom";
 
+
 const ResultPage = () => {
   const { mbti } = useParams();
 
   const resultMBTIData = resultData.find((data) => data.mbti === mbti);
 
   const resultDesc = resultMBTIData.desc.split("\n");
+
+  useEffect(() => {
+    if(!Kakao.isInitialized()) {
+        Kakao.init("9388c4ddefb150bf965706b3ec85dc51")
+    }
+  },[]);
+
+  const clickShareHandler = () => {
+    Kakao.Share.createCustomButton({
+        container: '#kakaotalk-sharing-btn',
+        templateId:977766,
+        templateArgs: {
+          THU: 'https://mbti-test-one.vercel.app' + resultMBTIData.image,
+          MATCH_CAT: resultMBTIData.match_cat,
+        },
+      });
+  }
 
   return (
     <div className={styles.layout}>
@@ -23,7 +41,11 @@ const ResultPage = () => {
           {desc}
         </p>
       ))}
-      
+
+
+      <button onClick={clickShareHandler} className={styles.square_button}>카카오톡 공유하기</button>
+      <Link to='/' className={styles.square_button}>다시해보기</Link>
+
     </div>
   );
 };
